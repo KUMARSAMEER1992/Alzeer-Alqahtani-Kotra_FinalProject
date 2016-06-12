557a603985ac71cfaf94c13abd16106d7c456a9a
sameer kumar <kumarsameer1992@gmail.com>
2016-06-12 16:56:23 -0500

    created armorAddon

---
+++ src/exception/ArmorException.java
@@ -0,0 +1,17 @@
+package exception;
+
+/**
+ * @author Sameer Kumar Kotra
+ */
+public class ArmorException extends Exception
+{
+
+	/**
+	 * @param string
+	 */
+	public ArmorException(String message)
+	{
+		super(message);
+	}
+
+}
--- src/items/armor/ArmorAddon.java
+++ src/items/armor/ArmorAddon.java
@@ -1,9 +1,68 @@
 package items.armor;
 
+import exception.ArmorException;
+import time.TimeObserver;
+
 /**
  * @author Sameer Kumar Kotra
  */
-public abstract class ArmorAddon
+public abstract class ArmorAddon implements Armor, TimeObserver
 {
+	/**
+	 * Armor to store armor.
+	 */
+	protected Armor armor;
+
+	/**
+	 * int to store max Rounds of Armor.
+	 */
+	private int maxRounds;
+
+	/**
+	 * int to store current round of Armor.
+	 */
+	private int currentRound;
+
+	public ArmorAddon(Armor armor, int maxRounds) throws ArmorException
+	{
+		int counter = 0;
+		Armor temp = armor;
+		currentRound = 0;
+		this.maxRounds = maxRounds;
+		while (true)
+		{
+			if (temp instanceof ArmorAddon)
+			{
+				temp = ((ArmorAddon) temp).armor;
+				counter++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (counter < 1)
+		{
+			this.armor = armor;
+		}
+		else
+		{
+			throw new ArmorException("You can add only 1 addon");
+		}
+	}
+
+	@Override
+	public void updateTime(int time)
+	{
+		currentRound++;
+	}
+
+	public boolean checkIsLive()
+	{
+		if (currentRound <= maxRounds)
+			return true;
+		else
+			return false;
+	}
 
 }
